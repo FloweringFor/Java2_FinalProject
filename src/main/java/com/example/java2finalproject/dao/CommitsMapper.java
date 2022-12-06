b4332ba2d1651979bc8b3e7441711f5e404feeef
FloweringFor <105793279+FloweringFor@users.noreply.github.com>
2022-12-06 20:59:56 +0800

    Delete src directory

--- src/main/java/com/example/java2finalproject/dao/CommitsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.java2finalproject.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.java2finalproject.entity.Commits;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CommitsMapper extends BaseMapper<Commits> {
-}
--- src/main/java/com/example/java2finalproject/service/IssuesService.java
+++
@@ -1,7 +0,0 @@
-package com.example.java2finalproject.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.java2finalproject.entity.Issues;
-
-public interface IssuesService extends IService<Issues> {
-}
