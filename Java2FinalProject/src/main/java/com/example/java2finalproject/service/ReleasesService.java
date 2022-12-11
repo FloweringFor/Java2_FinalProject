449c0fb37845beade1c110c20c6ec20fd26f8746
FloweringFor <105793279+FloweringFor@users.noreply.github.com>
2022-12-11 18:19:39 +0800

    Delete Java2FinalProject/src/main/java/com/example/java2finalproject directory

--- Java2FinalProject/src/main/java/com/example/java2finalproject/service/ReleasesService.java
+++
@@ -1,8 +0,0 @@
-package com.example.java2finalproject.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.java2finalproject.entity.Releases;
-
-public interface ReleasesService extends IService<Releases> {
-
-}
