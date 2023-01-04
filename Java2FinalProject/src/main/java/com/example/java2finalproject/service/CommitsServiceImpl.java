490c0625c8f1f3039dfa87f6c24590c07a4b6216
FloweringFor <105793279+FloweringFor@users.noreply.github.com>
2023-01-04 11:18:38 +0800

    Update CommitsServiceImpl.java

--- Java2FinalProject/src/main/java/com/example/java2finalproject/service/CommitsServiceImpl.java
+++ Java2FinalProject/src/main/java/com/example/java2finalproject/service/CommitsServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class CommitsServiceImpl extends ServiceImpl<CommitsMapper, Commits> implements CommitsService{
+public class CommitsServiceImpl extends ServiceImpl<CommitsMapper, Commits> implements CommitsService {
 
     @Autowired
     private CommitsMapper commitsMapper;
