c0742d3fff54f1a04f6311e1188ed9036de88dcc
FloweringFor <105793279+FloweringFor@users.noreply.github.com>
2023-01-04 11:17:08 +0800

    Update CommitsService.java

--- Java2FinalProject/src/main/java/com/example/java2finalproject/service/CommitsService.java
+++ Java2FinalProject/src/main/java/com/example/java2finalproject/service/CommitsService.java
@@ -4,7 +4,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.java2finalproject.entity.Commits;
 import java.util.List;
 
-public interface CommitsService extends IService<Commits>{
+public interface CommitsService extends IService<Commits> {
 
     List<Commits> listByRepo(String s);
 
