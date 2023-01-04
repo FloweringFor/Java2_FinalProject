56221426f647d81b6d34d7c94b70d38941d04e5f
FloweringFor <105793279+FloweringFor@users.noreply.github.com>
2023-01-04 11:19:00 +0800

    Update DevelopersServiceImpl.java

--- Java2FinalProject/src/main/java/com/example/java2finalproject/service/DevelopersServiceImpl.java
+++ Java2FinalProject/src/main/java/com/example/java2finalproject/service/DevelopersServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class DevelopersServiceImpl extends ServiceImpl<DevelopersMapper, Developers> implements DevelopersService{
+public class DevelopersServiceImpl extends ServiceImpl<DevelopersMapper, Developers> implements DevelopersService {
 
     @Autowired
     private DevelopersMapper developersMapper;
