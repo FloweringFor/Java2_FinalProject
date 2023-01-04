7e53dfb6b02868ec08e4f43b6a90e8ca5d2bdec7
FloweringFor <105793279+FloweringFor@users.noreply.github.com>
2023-01-04 11:20:59 +0800

    Update ReleasesServiceImpl.java

--- Java2FinalProject/src/main/java/com/example/java2finalproject/service/ReleasesServiceImpl.java
+++ Java2FinalProject/src/main/java/com/example/java2finalproject/service/ReleasesServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ReleasesServiceImpl extends ServiceImpl<ReleasesMapper, Releases> implements ReleasesService{
+public class ReleasesServiceImpl extends ServiceImpl<ReleasesMapper, Releases> implements ReleasesService {
 
     @Autowired
     private ReleasesMapper releasesMapper;
